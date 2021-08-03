b74daf0cbb4af85a5e405fef0fc74c1a97c83683
Harika <harikanarla52@gmail.com>
2021-08-03 09:44:59 -0500

    first commit

---
+++ ProjectForTest/src/ProjectA/SimpleJavaProgram.java
@@ -0,0 +1,19 @@
+package ProjectA;
+
+public class SimpleJavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=10;
+		int b=20;
+		
+		System.out.println("sum of a and b" +" " + (a+b));
+		
+		
+		
+		
+		
+	}
+
+}
